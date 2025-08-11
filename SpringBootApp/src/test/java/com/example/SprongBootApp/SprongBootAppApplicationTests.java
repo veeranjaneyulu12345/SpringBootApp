15964c9ebac3ad13a64a286ee2205115259114ba
veeranjaneyulu12345 <kammanethulaveeranjaneyulu@gmail.com>
2025-08-11 10:00:14 +0530

    Initial Spring Boot project

---
+++ SpringBootApp/src/test/java/com/example/SprongBootApp/SprongBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SprongBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprongBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
